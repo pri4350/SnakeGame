7b0e4ee4998571932fcfd92d264170ccab35994a
priyanka <priyankabhole21@gmail.com>
2026-07-21 21:03:48 +0530

    new

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const board = document.getElementById('board');
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 80;
+const blockWidth = 80;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = [];
 
 for (let i = 0; i < rows * cols; i++) {
     const block = document.createElement('div');
     block.classList.add('block');
     board.appendChild(block);
-
 }
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+    }
+}
+
