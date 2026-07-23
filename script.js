e6e19111e450db506743623a25d162b7275fcaa0
priyanka <priyankabhole21@gmail.com>
2026-07-23 12:56:38 +0530

    new

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const board = document.getElementById('board');
-const blockHeight = 80;
-const blockWidth = 80;
+const blockHeight = 50;
+const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = [];
+const snake = [{
+   x: 1, y: 3
+}, {
+    x: 1, y: 4
+}, {
+    x: 1, y:5
+} ]
 
-for (let i = 0; i < rows * cols; i++) {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
-}
+let direction = 'down';
+
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const block = document.createElement('div');
         block.classList.add('block');
         board.appendChild(block);
+        blocks[`${row}-${col}`] = block
+
     }
 }
+
+function render(){
+    snake.forEach(segment  => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+       
+    })
+
+}
+
+setInterval(() => {
+    render();
+}, 1000);
 
